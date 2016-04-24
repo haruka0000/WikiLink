2a76df731c0235116d90327fe2e9111b0db40c02
Haruka TAKAHASHI <laura.bodewig.marina@gmail.com>
2016-04-25 07:50:43 +0900

    prototype

--- test.py
+++ test.py
@@ -2,30 +2,45 @@ import Text
 import Titles
 
 if __name__ == '__main__':
-  start = "インフィニットストラトス"
+  #start = "有川浩"
+  #start = "インフィニット・ストラトス"
+  start = "高橋"
+  #start = "僕は友達が少ない"
   target = "井上麻里奈"
-  text = Text.get_wiki(start)[2]
-  words = Titles.get_titles(text)
-  print(words)
+  #text = Text.get_wiki(start)[2]  ## [id,title,text]
+  #words = Titles.get_titles(text)
+  #print(words)
+  
+  routes = []
+  result = ["### ERROR ###"]
 
-  i = 0
-  j = 0
-  all_words = set([])
+  routes.append([start])
 
-  while(True):
-    if target in words:
-      print("#### Find!! ####")
-      print(target)
+
+  while True:
+    next_routes = []
+    if target == str(result[-1]):
+      print(result)
+      print("終了！！！")
       break
-      
-    for w in words:
-      text = Text.get_wiki(w)[2]
-      sub_words = set(Titles.get_titles(text))
-      all_words = all_words.union(sub_words)
-      if target in sub_words:
-        print("#### Find!! ####")
-        print(target)
-        break
 
-    words = all_words
+    for r in routes:
+      text = Text.get_wiki(r[-1])[2]  ## [id,title,text]
+      words = Titles.get_titles(text)[:]
+      if target == str(result[-1]):
+        break
+      print("### ")
+      print(words)
+      for w in words:
+        new_route = []
+        new_route = r[:]
+        new_route.append(w)
+        next_routes.append(new_route)
+        print(new_route)
+        if target == str(new_route[-1]):
+          result = new_route[:]
+          break
+
+    routes = next_routes[:]     
+
 
