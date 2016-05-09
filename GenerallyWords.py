2a24da187b89de349b90fedfe42620cc620c34b6
Haruka TAKAHASHI <laura.bodewig.marina@gmail.com>
2016-05-10 01:11:26 +0900

    cycle work

--- GenerallyWords.py
+++ GenerallyWords.py
@@ -17,41 +17,42 @@ import urllib.request
 import Titles
 import time
 
-# OAuth2.0用のキーを取得する
-with open("secret.json") as f:
-  secretjson = json.load(f)
-
-
-# Twitterへの接続
-t = Twitter(auth=OAuth(secretjson["access_token"], secretjson["access_token_secret"], secretjson["consumer_key"], secretjson["consumer_secret"]))
-
 def twt_get(s):
-  limit = t.application.rate_limit_status()
-  for k,v in limit['resources']['search'].items():
-    print(k + " " + str(v))
-
-  if limit['resources']['search']['/search/tweets']['remaining'] == 1:
-    print("#### Waiting 15 minutes to recover access limit ####")
-    wait_m = 15
-    time.sleep(wait_m * 60)
-
-  print("## accessing... ##")
-  # 検索する
   try:
+    # OAuth2.0用のキーを取得する
+    with open("secret.json") as f:
+      secretjson = json.load(f)
+    
+    # Twitterへの接続
+    t = Twitter(auth=OAuth(secretjson["access_token"], secretjson["access_token_secret"], secretjson["consumer_key"], secretjson["consumer_secret"]))
+    
+    limit = t.application.rate_limit_status()
+    for k,v in limit['resources']['search'].items():
+      print(k + " " + str(v))
+    
+    if limit['resources']['search']['/search/tweets']['remaining'] == 1:
+      print("#### Waiting 15 minutes to recover access limit ####")
+      wait_m = 5
+      time.sleep(wait_m * 60)
+      
+    print("## accessing... ##")
+    
+    # 検索する
     t_words = t.search.tweets(q=s,count=100)
     return t_words
-  except Exception as e:
-    print("### エラー内容 ###")
-    print("type:" + str(type(e)))
-    print("args:" + str(e.args))
-    print("message:" + e.message)
-    print("e自身:" + str(e))
-    return "ERROR!!"
+  except:
+    return []
 
 def got_words(sample):
   words = []
   for s in sample:
-    twt = twt_get(s)
+    while True: 
+      twt = twt_get(s)
+      if len(twt) != 0:
+        break
+      print("#### Waiting 15 minutes to recover access limit ####")
+      wait_m = 5
+      time.sleep(wait_m * 60)
     for t in twt['statuses']:
       #print(t)
       text = t['text']
@@ -80,21 +81,37 @@ def general_words(sample):
   words = []
   s_words = wordsCount(sample)
   for sw in s_words:
-    if sw[1] >= 30:   # 30/100ツイートに含まれる
+    if sw[1] >= 40:   # 30/100ツイートに含まれる
       words.append(sw[0])
   return words
 
 
 if __name__ == '__main__':
-  f = open('General_words.txt', 'w') # 書き込みモードで開く 
-  sample = ["今日","最近","RT","私","拡散"]
-  for i in range(0,20):
-    sample = list(set(sample).union(set(general_words(sample))))
-  for s in sample:
-    f.write(s + ",") # 引数の文字列をファイルに書き込む
-  f.close() # ファイルを閉じる
-  print(sample)
+  count = 0
+  while count < 3:
+    f = open('General_words.txt', 'r')
+    filetext = ""
+    for line in f:
+      filetext = filetext + line
+    f.close()
+    
+    gen_words = filetext.split(",")[0:-1]
+    
+    bef_len = len(gen_words)
 
-#general_words(["今日"])
-#twt_words("井上麻里奈")
-#twt_words("#ann0")
+    if len(gen_words) == 0:
+      gen_words = ["今日","RT","私","自動","bot"]
+      
+    print(gen_words)
+    
+    gen_words = list(set(gen_words).union(set(general_words(gen_words))))
+    
+    f = open('General_words.txt', 'w') # 書き込みモードで開く 
+    for g in gen_words:
+      f.write(g + ",") # 引数の文字列をファイルに書き込む
+    f.close() # ファイルを閉じる
+    
+    aft_len = len(gen_words)
+
+    if bef_len == aft_len:
+      count = count + 1
